0c25d55e9fa840d84797ca14cc8cb5106cb66313
rafaeltorok <rafaeltorok@gmail.com>
2026-05-01 18:14:54 -0300

    Add support for Component Integrations tests with Vitest - add all basic libraries to support this - include a new separate configuration and setup scripts for Vitest to be able to work with React elements

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
@@ -11,9 +11,5 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
   },
 });
