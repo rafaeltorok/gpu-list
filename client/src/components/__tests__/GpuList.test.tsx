bb0cb26f1324d6e7c0955ca591849ec6a891a26c
rafaeltorok <rafaeltorok@gmail.com>
2026-05-07 12:43:25 -0300

    Add index scrolling and search filter tests to the suite

--- client/src/components/__tests__/GpuList.test.tsx
+++ client/src/components/__tests__/GpuList.test.tsx
@@ -1,6 +1,7 @@
 // Test dependencies
 import { describe, test, expect, beforeEach } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 // Component
 import GpuList from "../GpuList";
@@ -15,6 +16,9 @@ import GpuContext from "../../context/GpuContext";
 // TypeScript types
 import type { GpuType } from "../../../../shared/types/types";
 import type { GpuContextType } from "../../types/context";
+
+// Global variables
+let user: ReturnType<typeof userEvent.setup>;
 
 // Helper functions
 function createMockContextWithCards(gpus: GpuType[]): GpuContextType {
@@ -124,6 +128,198 @@ describe("Testing the Gpu List component", () => {
           name: /nvidia rtx pro 6000 blackwell/i,
         }),
       ).toBeInTheDocument();
+    });
+  });
+
+  describe("filtering the list based on search terms", () => {
+    test("only the filtered objects should be displayed", () => {
+      // Fetch all available sample cards
+      const gpus: GpuType[] = [
+        { ...sampleData.rtx5090, id: "rtx5090" },
+        { ...sampleData.rx9070xt, id: "rx9070xt" },
+        { ...sampleData.rx7900xtx, id: "rx7900xtx" },
+        { ...sampleData.gtx650, id: "gtx650" },
+        { ...sampleData.b580, id: "b580" },
+        { ...sampleData.g210, id: "g210" },
+      ];
+
+      // Filter only AMD Radeon cards
+      const filteredGpus: GpuType[] = [
+        { ...sampleData.rx9070xt, id: "rx9070xt" },
+        { ...sampleData.rx7900xtx, id: "rx7900xtx" },
+      ];
+
+      const mockContextValue = createMockContext();
+
+      const mockContextFiltered = {
+        ...mockContextValue,
+        dataState: {
+          ...mockContextValue.dataState,
+          gpus: gpus,
+          gpusFound: filteredGpus,
+        },
+        uiState: {
+          ...mockContextValue.uiState,
+          searchGpu: "radeon",
+        },
+      };
+
+      // Render the list component, containing only the filtered objects
+      render(
+        <GpuContext.Provider value={mockContextFiltered}>
+          <GpuList />
+        </GpuContext.Provider>,
+      );
+
+      // Confirm there are only two objects being displayed on the list
+      expect(screen.getAllByTestId("gpu-data-table").length).toBe(2);
+
+      // Assert only the AMD Radeon cards are present on the list
+      expect(
+        screen.getByRole("columnheader", {
+          name: /amd radeon rx 9070 xt/i,
+        }),
+      ).toBeInTheDocument();
+
+      expect(
+        screen.getByRole("columnheader", {
+          name: /amd radeon rx 7900 xtx/i,
+        }),
+      ).toBeInTheDocument();
+
+      // Assert none of the NVIDIA and Intel cards are present
+      expect(
+        screen.queryByRole("columnheader", {
+          name: /nvidia geforce rtx 5090/i,
+        }),
+      ).not.toBeInTheDocument();
+
+      expect(
+        screen.queryByRole("columnheader", {
+          name: /nvidia geforce gtx 650/i,
+        }),
+      ).not.toBeInTheDocument();
+
+      expect(
+        screen.queryByRole("columnheader", {
+          name: /intel arc b580/i,
+        }),
+      ).not.toBeInTheDocument();
+
+      expect(
+        screen.queryByRole("columnheader", {
+          name: /nvidia geforce 210/i,
+        }),
+      ).not.toBeInTheDocument();
+    });
+
+    test("should display a message when no cards have been found", () => {
+      // Fetch all available sample cards
+      const gpus: GpuType[] = [
+        { ...sampleData.rtx5090, id: "rtx5090" },
+        { ...sampleData.rx9070xt, id: "rx9070xt" },
+        { ...sampleData.rx7900xtx, id: "rx7900xtx" },
+        { ...sampleData.gtx650, id: "gtx650" },
+        { ...sampleData.b580, id: "b580" },
+        { ...sampleData.g210, id: "g210" },
+      ];
+
+      const mockContextValue = createMockContext();
+
+      const mockContextFiltered = {
+        ...mockContextValue,
+        dataState: {
+          ...mockContextValue.dataState,
+          gpus: gpus,
+          gpusFound: [],
+        },
+        uiState: {
+          ...mockContextValue.uiState,
+          searchGpu: "none",
+        },
+      };
+
+      // Render the list component, containing only the filtered objects
+      render(
+        <GpuContext.Provider value={mockContextFiltered}>
+          <GpuList />
+        </GpuContext.Provider>,
+      );
+
+      // Confirm there are no objects being displayed on the list
+      expect(screen.queryAllByTestId("gpu-data-table").length).toBe(0);
+
+      // Assert a message is present on the page
+      expect(screen.getByText(/no gpus found/i)).toBeInTheDocument();
+    });
+  });
+
+  describe("the back to index button", () => {
+    beforeEach(() => {
+      // Setup the user event
+      user = userEvent.setup();
+    });
+
+    test("clicking on the button should automatically hide a data table", async () => {
+      const gpu: GpuType[] = [{ ...sampleData.rtx5090, id: "rtx5090" }];
+
+      const mockContextWithCards = createMockContextWithCards(gpu);
+
+      render(
+        <GpuContext.Provider value={mockContextWithCards}>
+          <GpuList />
+        </GpuContext.Provider>,
+      );
+
+      // Select the entire GPU data section
+      const dataTable = screen.getByRole("region", {
+        name: /nvidia geforce rtx 5090/i,
+      });
+
+      // Click on the Show button to display all the data
+      const showButton = within(dataTable).getByRole("button", {
+        name: /show/i,
+      });
+      await user.click(showButton);
+
+      // Confirm the table has been opened
+      expect(
+        within(dataTable).getByRole("columnheader", {
+          name: /specifications/i,
+        }),
+      ).toBeInTheDocument();
+      expect(
+        within(dataTable).getByRole("columnheader", { name: /clock speeds/i }),
+      ).toBeInTheDocument();
+      expect(
+        within(dataTable).getByRole("columnheader", {
+          name: /theoretical performance/i,
+        }),
+      ).toBeInTheDocument();
+
+      // Select its respective back to index button
+      const backToIndexButton = within(dataTable).getByRole("button", {
+        name: /back to index/i,
+      });
+
+      await user.click(backToIndexButton);
+
+      // Confirm the table has closed
+      expect(
+        within(dataTable).queryByRole("columnheader", {
+          name: /specifications/i,
+        }),
+      ).not.toBeInTheDocument();
+      expect(
+        within(dataTable).queryByRole("columnheader", {
+          name: /clock speeds/i,
+        }),
+      ).not.toBeInTheDocument();
+      expect(
+        within(dataTable).queryByRole("columnheader", {
+          name: /theoretical performance/i,
+        }),
+      ).not.toBeInTheDocument();
     });
   });
 });
