a30f40f802353302429e5cad04d743f67e31821d
rafaeltorok <rafaeltorok@gmail.com>
2026-05-11 16:20:41 -0300

    Add more guard checks for the util - implement another regex to remove duplicate whitespaces form the output

--- shared/utils/generateGpuDomId.ts
+++ shared/utils/generateGpuDomId.ts
@@ -8,12 +8,14 @@ export default function generateGpuDomId(gpu: GpuType | GpuInputType): string {
   }
 
   if (
+    !gpu.manufacturer ||
     !gpu.manufacturer.trim() ||
+    !gpu.model ||
     !gpu.model.trim()
   ) {
     return "";
   }
 
   const fullModelName = `${gpu.manufacturer.trim()}-${gpu.gpuline.trim()}-${gpu.model.trim()}`;
-  return fullModelName.replace(/\s+/g, '-').toLowerCase();
+  return fullModelName.replace(/\s+/g, '-').replace(/-{2,}/g, '-').toLowerCase();
 }
