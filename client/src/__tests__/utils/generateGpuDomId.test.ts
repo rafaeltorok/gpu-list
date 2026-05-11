d79cc8481dd05a6a4c75f086b3140ed05354fd06
rafaeltorok <rafaeltorok@gmail.com>
2026-05-11 16:20:59 -0300

    Add unit test suite for the util script

---
+++ client/src/__tests__/utils/generateGpuDomId.test.ts
@@ -0,0 +1,100 @@
+// Vitest
+import { describe, expect, test } from "vitest";
+
+// Util
+import generateGpuDomId from "../../../../shared/utils/generateGpuDomId";
+
+// Test data
+import sampleData from "../../test-utils/data/fixtures";
+
+//Tests
+describe("Testing the generate GPU DOM ID util", () => {
+  describe("valid inputs", () => {
+    test("a valid GPU object should return a proper id", () => {
+      const gpu = sampleData.rtx5090;
+
+      const domId = generateGpuDomId(gpu);
+
+      expect(domId).toBe("nvidia-geforce-rtx-5090");
+    });
+
+    test("the gpuline field is optional", () => {
+      const gpu = {
+        ...sampleData.rtx5090,
+        gpuline: "",
+      };
+
+      const domId = generateGpuDomId(gpu);
+
+      expect(domId).toBe("nvidia-rtx-5090");
+    });
+
+    test("the output should not contain leading and trailing whitespaces", () => {
+      const gpu = {
+        ...sampleData.rtx5090,
+        manufacturer: "  NVIDIA ",
+        gpuline: "  GeForce  ",
+        model: "  RTX 5090   ",
+      };
+
+      const domId = generateGpuDomId(gpu);
+
+      expect(domId).toBe("nvidia-geforce-rtx-5090");
+    });
+
+    test("multiple whitespaces should be converted into a single dash", () => {
+      const gpu = {
+        ...sampleData.rx9070xt,
+        model: "   RX    9070   XT",
+      };
+
+      const domId = generateGpuDomId(gpu);
+
+      expect(domId).toBe("amd-radeon-rx-9070-xt");
+    });
+  });
+
+  describe("invalid inputs", () => {
+    test("an empty manufacturer field", () => {
+      const gpu = {
+        ...sampleData.rtx5090,
+        manufacturer: "",
+      };
+
+      const domId = generateGpuDomId(gpu);
+
+      // Confirm only an empty string is returned
+      expect(domId).toBe("");
+    });
+
+    test("an empty model field", () => {
+      const gpu = {
+        ...sampleData.rtx5090,
+        model: "",
+      };
+
+      const domId = generateGpuDomId(gpu);
+
+      // Confirm only an empty string is returned
+      expect(domId).toBe("");
+    });
+
+    test("passing no input value", () => {
+      // @ts-expect-error
+      const domId = generateGpuDomId();
+
+      // Confirm only an empty string is returned
+      expect(domId).toBe("");
+    });
+
+    test("malformed input value", () => {
+      const { manufacturer, model, ...otherFields } = sampleData.rtx5090;
+
+      // @ts-expect-error
+      const domId = generateGpuDomId(otherFields);
+
+      // Confirm only an empty string is returned
+      expect(domId).toBe("");
+    });
+  });
+});
